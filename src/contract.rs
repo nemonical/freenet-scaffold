545e822fb77fb19df7937ca6f135dc50aedc7aaa
nemonical <admin@nemonical.art>
2025-05-13 23:07:16 +0200

    I fixed some issues I noticed, and implemented some `todo!()`s

--- src/contract.rs
+++ src/contract.rs
@@ -5,11 +5,14 @@ use std::cell::RefCell;
 use crate::ComposableState;
 
 pub struct Contract<T>(std::marker::PhantomData<T>)
-    where T: ComposableState;
-
-impl<T> Contract<T> 
 where
-    T: ComposableState<ParentState = RefCell<RelatedContracts<'static>>> + Serialize + for<'a> Deserialize<'a>
+    T: ComposableState;
+
+impl<T> Contract<T>
+where
+    T: ComposableState<ParentState = RefCell<RelatedContracts<'static>>>
+        + Serialize
+        + for<'a> Deserialize<'a>,
 {
     // TODO: Is there a better way to give `ComposableState`s the ability to get related contracts
     // than setting its `ParentState` to `RefCell<RelatedContracts<'static>>` and giving it dummy
@@ -23,20 +26,47 @@ where
         state: State<'static>,
         related: RelatedContracts<'static>,
     ) -> Result<ValidateResult, ContractError> {
+        let related = RefCell::new(related);
+
         match T::verify(
             &match serde_json::from_slice(state.as_ref()) {
                 Ok(state) => state,
-                // TODO: write an error message here?
-                Err(error) => return Err(ContractError::Deser(format!("{}", error))),
+                Err(error) => {
+                    return Err(ContractError::Deser(format!(
+                        "an error occured while deserializing the contract state: {}",
+                        error
+                    )))
+                }
             },
-            &RefCell::new(related),
+            &related,
             &match serde_json::from_slice(parameters.as_ref()) {
                 Ok(parameters) => parameters,
-                // TODO: write an error message here?
-                Err(error) => return Err(ContractError::Deser(format!("{}", error)))
+                Err(error) => {
+                    return Err(ContractError::Deser(format!(
+                        "an error occured while deserializing the contract parameters: {}",
+                        error
+                    )))
+                }
             },
         ) {
-            Ok(_) => Ok(ValidateResult::Valid),
+            Ok(_) => {
+                let missing_contracts = related
+                    .borrow()
+                    .states()
+                    .filter_map(|(id, state)| {
+                        if state.is_none() {
+                            Some(id.to_owned())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<_>>();
+                if missing_contracts.is_empty() {
+                    Ok(ValidateResult::Valid)
+                } else {
+                    Ok(ValidateResult::RequestRelated(missing_contracts))
+                }
+            }
             Err(_) => Ok(ValidateResult::Invalid),
         }
     }
@@ -44,14 +74,34 @@ where
 
 impl<T, P> Contract<T>
 where
-    T: ComposableState<ParentState = P>,
+    T: ComposableState<ParentState = P> + Serialize + for<'a> Deserialize<'a>,
     P: Default,
-{ 
+{
     pub fn summarize_state(
         parameters: Parameters<'static>,
         state: State<'static>,
     ) -> Result<StateSummary<'static>, ContractError> {
-        match serde_json::to_vec(&T::summarize(todo!(), &T::ParentState::default(), todo!())) {
+        match serde_json::to_vec(&T::summarize(
+            &match serde_json::from_slice(state.as_ref()) {
+                Ok(state) => state,
+                Err(error) => {
+                    return Err(ContractError::Deser(format!(
+                        "an error occured while deserializing the contract state: {}",
+                        error
+                    )))
+                }
+            },
+            &T::ParentState::default(),
+            &match serde_json::from_slice(parameters.as_ref()) {
+                Ok(parameters) => parameters,
+                Err(error) => {
+                    return Err(ContractError::Deser(format!(
+                        "an error occured while deserializing the contract parameters: {}",
+                        error
+                    )))
+                }
+            },
+        )) {
             Ok(summary) => Ok(summary.into()),
             Err(_) => todo!(),
         }
@@ -62,7 +112,36 @@ where
         state: State<'static>,
         summary: StateSummary<'static>,
     ) -> Result<StateDelta<'static>, ContractError> {
-        match serde_json::to_vec(&T::delta(todo!(), &T::ParentState::default(), todo!(), todo!())) {
+        match serde_json::to_vec(&T::delta(
+            &match serde_json::from_slice(state.as_ref()) {
+                Ok(state) => state,
+                Err(error) => {
+                    return Err(ContractError::Deser(format!(
+                        "an error occured while deserializing the contract state: {}",
+                        error
+                    )))
+                }
+            },
+            &T::ParentState::default(),
+            &match serde_json::from_slice(parameters.as_ref()) {
+                Ok(parameters) => parameters,
+                Err(error) => {
+                    return Err(ContractError::Deser(format!(
+                        "an error occured while deserializing the contract parameters: {}",
+                        error
+                    )))
+                }
+            },
+            &match serde_json::from_slice(summary.as_ref()) {
+                Ok(summary) => summary,
+                Err(error) => {
+                    return Err(ContractError::Deser(format!(
+                        "an error occurred while deserializing the contract summary: {}",
+                        error
+                    )))
+                }
+            },
+        )) {
             Ok(delta) => Ok(delta.into()),
             Err(_) => todo!(),
         }
@@ -73,9 +152,16 @@ where
         state: State<'static>,
         data: Vec<UpdateData<'static>>,
     ) -> Result<UpdateModification<'static>, ContractError> {
-        match T::apply_delta(todo!(), &T::ParentState::default(), todo!(), todo!()) {
-            Ok(()) => todo!(),
-            Err(_) => todo!(),
-        }
+        let mut state: T = match serde_json::from_slice(state.as_ref()) {
+            Ok(state) => state,
+            Err(error) => {
+                return Err(ContractError::Deser(format!(
+                    "an error occured while deserializing the contract state: {}",
+                    error
+                )))
+            }
+        };
+
+        todo!()
     }
 }
